fd6988476f04bb8c6ae19f7610bc75e39f4b3252
Danilo Patanè <d.patane98@gmail.com>
2020-08-03 15:34:28 +0200

    Correction

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 // chiedo il nome all'utente
 var nome = prompt("Inserisci il tuo nome");
-var nomePars = parseInt(nome);
 // chiedo il cognome
 var cognome = prompt("Inserisci il tuo cognome");
-var cognomePars = parseInt(cognome);
 //chiedo il colore preferito
 var colorePreferito = prompt("Inserisci il tuo colorePreferito");
-var colorePreferitoPars = parseInt(colorePreferito);
 // Eseguo la somma e aggiungo 19
 var somma = nome + cognome + colorePreferito + "19";
 // Stampo
